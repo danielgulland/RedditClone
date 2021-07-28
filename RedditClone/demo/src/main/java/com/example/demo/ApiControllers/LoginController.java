ecd0e63aaead6ee90b91c2a85df2c073e81ef46c
NateTheGreat123 <nathanjsmith95@gmail.com>
2021-07-28 13:25:22 -0700

    email verification and validation. Username and Password validation

--- RedditClone/demo/src/main/java/com/example/demo/ApiControllers/LoginController.java
+++ RedditClone/demo/src/main/java/com/example/demo/ApiControllers/LoginController.java
@@ -8,7 +8,6 @@ import com.example.demo.Validation.ValidationError;
 import com.example.demo.Validation.Validator;
 import com.mysql.cj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
